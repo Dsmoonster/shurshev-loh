64974d88a8bdeec7901eac479112f8e80294bc6a
DSLOH <mars228@mail.ru>
2022-06-05 15:43:38 +0300

    Add basket

---
+++ resources/views/basket.blade.php
@@ -0,0 +1,94 @@
+
+                <main>
+                    <div class="lkb">
+                         <div class="menu">
+                             <button onclick="window.location.href = 'lkb.html'">Личные данные</button>
+                             <button onclick="window.location.href = 'adress.html'">Мои заказы</button>
+                             <button onclick="window.location.href = 'index.html'">История заказов</button>
+                             <button onclick="window.location.href = 'korzina.html'">Корзина</button><br><br>
+                             <button class="exit_button" onclick="window.location.href = 'index.html'">Выход</button>
+                         </div>
+                     <div class="info_lkb">
+                         <p class="zakaz_up">Моя корзина</p>
+                         <div class="info_zakaz">
+
+                             <div class="info_zakaz_status">
+                                 <div class="dano">
+                                      <h2>Информация о заказе</h2><br>
+                                          <p class="kor_naz">Руль анатомический Lada Priora 2 (перфорация, кожа, цветная строчка) Россия</p><br>
+                                          <p class="code">Код товара:412321</p>
+                                 </div>
+                                 <!-- <div class="dano"><br><br><br>
+                                     <p>Текущий заказ:</p><br>
+                                     <p>Заказ отменен</p>
+                                 </div> -->
+                                 <div class="dano"><br><br><br>
+                                     <p>Сумма:</p><br>
+                                     <p>604 ₽</p>
+                                 </div>
+                                 <div class="dano"><br><br><br>
+                                     <button>Удалить</button>
+                                 </div>
+
+                             </div>
+                              <div class="info_zakaz_status">
+                                <div class="dano">
+                                    <h2>Информация о заказе</h2><br>
+                                        <p class="kor_naz">Руль анатомический Lada Priora 2 (перфорация, кожа, цветная строчка) Россия</p><br>
+                                        <p class="code">Код товара:412321</p>
+                               </div>
+                                 <!-- <div class="dano"><br><br><br>
+                                     <p>Текущий заказ:</p><br>
+                                     <p>Заказ отменен</p>
+                                 </div> -->
+                                 <div class="dano"><br><br><br>
+                                     <p>Сумма:</p><br>
+                                     <p>604 ₽</p>
+                                 </div>
+                                 <div class="dano"><br><br><br>
+                                     <button>Удалить</button>
+                                 </div>
+                             </div>
+                             <div class="info_zakaz_status">
+                                <div class="dano">
+                                    <h2>Ваш адресс</h2><br>
+                                     <input type="text" name="city" placeholder="Ваш город">
+                                     <input type="text" name="street" placeholder="Ваша улица"><br>
+                                     <input type="text" name="home" placeholder="№ дома">
+                                     <input type="text" name="home_nomer" placeholder="№ Квартиры(Если есть)">
+                                </div>
+                                 <div class="dano"><br><br><br>
+                                     <p>Сумма:</p><br>
+                                     <p>1208 ₽</p>
+                                 </div>
+                                 <div class="dano_ob"><br><br><br>
+                                     <button class="oform">Оформить</button>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                    </div>
+
+         </main>
+         <footer>
+        <div class="info">
+            <h1>Навигация</h1><br>
+            <a href="oplata.html">Оплата</a><br>
+            <a href="#">Контакты</a><br>
+            <a href="#">Доставка</a><br>
+        </div>
+        <div class="info_block_f">
+            <h1>Помощь</h1><br>
+            <a href="oplata.html">Условия оплаты</a><br>
+            <a href="dostavka.html">Условия доставки</a><br>
+            <a href="garant.html">Гарантия на товар</a>
+               </div>
+        <div class="info_block_f">
+            <h1>Наши контакты</h1><br>
+            <p>+7(999)164 39 92</p>
+        <p>autoget@mail.com</p>
+
+        </div>
+    </footer>
+</body>
+</html>
